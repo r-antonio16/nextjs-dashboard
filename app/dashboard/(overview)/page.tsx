be1c3dde4274cf18401f496a3f30e4d2af264905
r-antonio16 <47504810+r-antonio16@users.noreply.github.com>
2024-03-05 15:46:14 +0000

    complete streaming chapter

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -13,26 +13,18 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      {/*<Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
-        </Suspense> */}
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
 
-        {/* <Suspense fallback={<RevenueChartSkeleton />}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
-      </Suspense> */}
-        {/* <Suspense fallback={<LatestInvoicesSkeleton />}>
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
-      </Suspense> */}
+        </Suspense>
       </div>
     </main>
   );
